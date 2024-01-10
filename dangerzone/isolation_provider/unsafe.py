a284fc94cd314cc743c60a0e38c80b087e4cd271
deeplow <deeplower@protonmail.com>
2024-01-10 12:09:06 +0000

    FIXUP: remove old Qubes references in Unsafe

--- dangerzone/isolation_provider/unsafe.py
+++ dangerzone/isolation_provider/unsafe.py
@@ -33,32 +33,14 @@ class UnsafeConverter(Qubes):
         return True
 
     def start_doc_to_pixels_proc(self) -> subprocess.Popen:
-        dev_mode = getattr(sys, "dangerzone_dev", False) == True
-        if dev_mode:
-            # Use dz.ConvertDev RPC call instead, if we are in development mode.
-            # Basically, the change is that we also transfer the necessary Python
-            # code as a zipfile, before sending the doc that the user requested.
-            qrexec_policy = "dz.ConvertDev"
-            stderr = subprocess.PIPE
-        else:
-            qrexec_policy = "dz.Convert"
-            stderr = subprocess.DEVNULL
-
-        p = subprocess.Popen(
+        return subprocess.Popen(
             # XXX The unsafe converter bypasses the isolation provider by calling
             # the Qubes server component directly
-            [Path(__file__).parent.parent.parent / "qubes" / qrexec_policy],
+            [Path(__file__).parent.parent.parent / "qubes" / "dz.ConvertDev"],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
-            stderr=stderr,
+            stderr=subprocess.PIPE,
         )
-
-        if dev_mode:
-            assert p.stdin is not None
-            # Send the dangerzone module first.
-            self.teleport_dz_module(p.stdin)
-
-        return p
 
     def get_max_parallel_conversions(self) -> int:
         return 1
